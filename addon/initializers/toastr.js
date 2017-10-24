87197165e2c043e446092146a7849fa31ebc0dd1
Jan-Philipp Dombrowski <jpdombrowski@gmail.com>
2017-10-24 16:23:30 +0200

    Check for FastBoot environment

--- addon/initializers/toastr.js
+++ addon/initializers/toastr.js
@@ -1,8 +1,11 @@
 export function initialize(application, options) {
-  var injectAs = options.injectAs;
-  window.toastr.options = options.toastrOptions;
+  if (typeof FastBoot === 'undefined') {
+    const injectAs = options.injectAs;
 
-  application.inject('route', injectAs, 'service:toast');
-  application.inject('controller', injectAs, 'service:toast');
-  application.inject('component', injectAs, 'service:toast');
+    window.toastr.options = options.toastrOptions;
+
+    application.inject('route', injectAs, 'service:toast');
+    application.inject('controller', injectAs, 'service:toast');
+    application.inject('component', injectAs, 'service:toast');
+  }
 }
